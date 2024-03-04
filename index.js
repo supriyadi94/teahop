0d3b1d3e1b51f6a21a45032b7647d95426c131a2
supriyadi94 <89642759+supriyadi94@users.noreply.github.com>
2024-03-04 20:23:53 +0000

    gold code

---
+++ index.js
@@ -0,0 +1,9 @@
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+  }
+  
+  // Example of usage:
+  const celsiusTemperature = 25;
+  const fahrenheitTemperature = celsiusToFahrenheit(celsiusTemperature);
+  console.log(`${celsiusTemperature}°C is equal to ${fahrenheitTemperature}°F`);
+  
